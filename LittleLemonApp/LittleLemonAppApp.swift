32b716b7565e592bbf53bb40aec078e0f848d91c
sgsjha <sarthakjhaa11@gmail.com>
2024-08-06 18:18:33 +0530

    Initial Commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Sarthak Jha on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
